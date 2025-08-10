9fd3114342c85388196a305f6dc0ac6f945cb196
Diego <coradd4@gmail.com>
2025-08-10 18:16:17 -0300

    4.Condicionales++ ejercicios terminados

---
+++ 4.Condicionales++/ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,17 @@
+﻿//4. Hacer un programa para ingresar tres números y emitir un cartel aclaratorio si la suma de los dos primeros es mayor al producto del segundo con el tercero.
+
+
+int n1 = 0, n2 = 0, n3 = 0;
+Console.Write("Ingrese un numero: ");
+n1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un segundo numero: ");
+n2 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un tercer numero: ");
+n3 = int.Parse(Console.ReadLine());
+if ((n1 + n2) > (n2 * n3))
+{
+    Console.WriteLine("La SUMA es mayor");
+}else
+{
+    Console.WriteLine("El PRODUCTO es mayor");
+}
---
+++ 4.Condicionales++/ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,19 @@
+﻿//5. Hacer un programa para ingresar 4 números. Luego analizar e informar por pantalla si los mismos se encuentran ordenados de forma decreciente .
+
+
+int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+Console.Write("Ingrese un numero: ");
+n1 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un SEGUNDO numero: ");
+n2 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un TERCER numero: ");
+n3 = int.Parse(Console.ReadLine());
+Console.Write("Ingrese un CUARTO numero: ");
+n4 = int.Parse(Console.ReadLine());
+if (n1 > n2 && n2 > n3 && n3 > n4)
+{
+    Console.WriteLine("Los numeros estan ordenados de forma DECRECIENTE");
+}else
+{
+    Console.WriteLine("NO estan ordenados de forma decreciente...");
+}
